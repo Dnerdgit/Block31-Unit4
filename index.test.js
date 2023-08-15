565271cf0f88148f8665b4f9fb7548e96b51936a
DuBoris Smith <duboris.smith@hotmail.com>
2023-08-15 17:42:47 -0500

    Unit4 code app.get functions added

--- index.test.js
+++ index.test.js
@@ -3,14 +3,26 @@ const pets = require('/data.js');
 
 const app = express();
 
+const PORT = 8080;
+
 app.get('/', (req, res) => { 
-    const { homepage } = req.query;
-    res.send(`Returns homepage: ${homepage}`)
+    res.sendFile(_dirname + '/public/index.html')
 
 });
 
+// hello world route
+app.get('/api', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/api/v1/pets', (req, res) => {
+    // const { pets } = req.query;
+    // res.send(`Returns all pets: ${pets}`)
+    res.send(pets);
+})
+
 app.get('/api/v1/pets/:name', (req, res) => {
-    const name = req.params.name;
+    const name = req.query.name;
     const filterPetName = data.filter((pet) => pet.name === name)
     console.log(filterPetName);
 
@@ -19,19 +31,15 @@ app.get('/api/v1/pets/:name', (req, res) => {
 })
 
 app.get('/api/v1/pets/:owner', (req, res) => {
-    const owner = req.params.owner;
+    const owner = req.query.owner;
     const filterOwner = data.filter((pet) => pet.owner === owner);
 
     res.send(filterOwner);
     // res.send(`Returns all pets by owner: ${pets}, ${owner}`)
 })
 
-app.get('/api/v1/pets', (req, res) => {
-    // const { pets } = req.query;
-    // res.send(`Returns all pets: ${pets}`)
-    res.send(pets);
-})
-
 app.listen(PORT, () => {
     console.log(`Listen on port ${PORT}`);
 })
+
+module.exports = app;
