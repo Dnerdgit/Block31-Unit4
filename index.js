0a76a74ccc868a988d45ebb4ff9934493d5feb37
DuBoris Smith <duboris.smith@hotmail.com>
2023-08-22 19:22:27 -0500

    Corrected code

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// import the pets array from data.js
-const pets = require('./data');
-
-// init express app
 const express = require('express');
+const pets = require('./data.js');
+
 const app = express();
 
 const PORT = 8080;
 
-// GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
+app.get('/', (req, res) => { 
+    res.sendFile(_dirname + '/public/index.html')
 
 });
 
@@ -18,38 +15,31 @@ app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
 
-// get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
+    // const { pets } = req.query;
+    // res.send(`Returns all pets: ${pets}`)
+    res.send(pets);
+})
 
-});
-
-// get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
+    const name = req.params.name;
+    const filterPetName = pets.filter((pet) => pet.name.toLowerCase() === name.toLowerCase())
+    console.log(filterPetName);
 
+    res.send(filterPetName);
+    // res.send(`Returns all pets by name: ${pets}, ${name}`)
+})
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+app.get('/api/v1/pets/:owner', (req, res) => {
+    const owner = req.query.owner;
+    const filterOwner = pets.filter((pet) => pet.owner === owner);
 
-    // send the pet as a response
-
-});
-
-// get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
-
-
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
-
-    // send the pet as a response
-
-});
+    res.send(filterOwner);
+    // res.send(`Returns all pets by owner: ${pets}, ${owner}`)
+})
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
-});
+    console.log(`Listen on port ${PORT}`);
+})
 
 module.exports = app;
